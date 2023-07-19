c623f1588b8f3ef44633f2841fa7187d416de71c
Paul Thomas <paulthomas2107@gmail.com>
2023-07-19 20:37:39 +0100

    More

--- src/main/java/com/pault/example/finalsection/Main.java
+++ src/main/java/com/pault/example/finalsection/Main.java
@@ -13,6 +13,8 @@ public class Main {
 
         hello("Paul", null, value -> log.info("No Lastname provided for " + value));
         hello("Rory", "Thomas", value -> log.info("No Lastname provided for " + value));
+        hello2("Rory", "Thomas", () -> log.info("No Lastname provided"));
+        hello2("Caitlin", null, () -> log.info("No Lastname provided"));
     }
 
     static void hello(String firstName, String lastName, Consumer<String> callback) {
@@ -21,6 +23,15 @@ public class Main {
             log.info(lastName);
         } else {
             callback.accept(firstName);
+        }
+    }
+
+    static void hello2(String firstName, String lastName, Runnable callback) {
+        log.info(firstName);
+        if (lastName != null) {
+            log.info(lastName);
+        } else {
+            callback.run();
         }
     }
 }
