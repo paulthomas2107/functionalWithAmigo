96280862984dc974c9b9b0aefb72313560a45730
Paul Thomas <paulthomas2107@gmail.com>
2023-07-19 19:41:18 +0100

    More

--- src/main/java/com/pault/example/combinatorpattern/Main.java
+++ src/main/java/com/pault/example/combinatorpattern/Main.java
@@ -4,6 +4,7 @@ import lombok.extern.java.Log;
 
 import java.time.LocalDate;
 
+
 @Log
 public class Main {
     public static void main(String[] args) {
@@ -11,11 +12,27 @@ public class Main {
         Customer customer = new Customer(
                 "Alice",
                 "alice@gmail.com",
-                "+440798222222",
+                "+940798222222",
                 LocalDate.of(2000, 1, 1)
         );
 
         log.info(String.valueOf(new CustomerValidatorService().isValid(customer)));
+
+        // If Customer is valid can be stored in DB....
+
+        // Using combination pattern
+        CustomerRegistrationValidator.ValidationResult result = CustomerRegistrationValidator
+                .isEmailValid()
+                .and(CustomerRegistrationValidator.isPhoneNumberValid())
+                .and(CustomerRegistrationValidator.isAnAdult())
+                .apply(customer);
+
+        log.info(String.valueOf(result));
+
+        if (result != CustomerRegistrationValidator.ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
+
 
     }
 }
